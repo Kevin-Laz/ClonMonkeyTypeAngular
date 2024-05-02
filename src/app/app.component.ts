10d4f01090685c1f2419484773fd9e8d30c7fb29
Kevin-Laz <Kevin-Laz@outlook.com>
2024-05-02 13:23:38 -0500

    Se define letras y parrafo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CuerpoComponent } from "./cuerpo/cuerpo.component";
+import { WordComponent } from './word/word.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CuerpoComponent, WordComponent]
 })
 export class AppComponent {
   title = 'ClonMonkeyTypeAngular';
--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-cuerpo',
-  standalone: true,
-  imports: [],
-  templateUrl: './cuerpo.component.html',
-  styleUrl: './cuerpo.component.css'
+    selector: 'app-cuerpo',
+    standalone: true,
+    templateUrl: './cuerpo.component.html',
+    styleUrl: './cuerpo.component.css',
+    imports: [CommonModule]
 })
-export class CuerpoComponent {
+export class CuerpoComponent implements OnInit{
+  ngOnInit(): void {
+    this.Inicio();
+  }
+  TIEMPO_BASE:Number = 50;
+  TEXTO_BASE:String = "árbol gato perro casa sol libro manzana papel ventana tierra agua luz puerta coche mesa silla aire vida noche día cielo camino amor verano invierno primavera otoño fuego mar montaña nube flor luna estrella tiempo aire boca nariz ojo oreja mano pie cabeza corazón cuerpo alma mente pensamiento palabra voz música arte dibujo pintura escultura poesía canción baile juego trabajo";
 
+  palabras:Array<String> = [];
+  texto:String = '';
+  tiempoActual = this.TIEMPO_BASE;
+  Inicio(){
+    this.tiempoActual = this.TIEMPO_BASE;
+    this.palabras = this.TEXTO_BASE.split(' ');
+    //this.palabras.map(p=>this.texto = this.texto.concat('<span>'+ p +" "+ '</span>'))
+    //console.log(this.texto)
+  }
 }
